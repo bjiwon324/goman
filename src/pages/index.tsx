75bb900bb4b9b5aa8367e034c8a953bb07df856b
sobokki <jso4093@naver.com>
2024-06-26 14:13:04 +0900

    Revert "사이드바 UI 구현"
    This reverts commit 8d55e829c2b2721b9ecdca296126eb4f7e671052.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import SideBar from "@/components/common/sideBar";
-import PageNation from "@/components/pagenationBtn/pagenation";
 export default function Home() {
   return (
     <>
       <h1>고만해</h1>
-      <SideBar/>
-      {/* <PageNation/> */}
     </>
   );
 }
