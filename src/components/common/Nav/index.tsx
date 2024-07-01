e16943a1d61ef0854c05ccec209bd2792a6417b4
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-07-01 22:00:15 +0900

    fix: nav ui 수정

--- src/components/common/Nav/index.tsx
+++ src/components/common/Nav/index.tsx
@@ -11,11 +11,15 @@ import Link from "next/link";
 
     return(
         <nav className="w-[100vw] h-[250px] sticky top-0 flex flex-col justify-center items-center bg-violet-200 mr-auto z-10" >
-          <div className=" flex flex-col justify-center items-center relative gap-[30px] py-[25px]">
+          <div className=" flex flex-col justify-center items-center gap-[30px] py-[25px]">
            <h1 className={`text-zinc-500 text-[52px] text-center w-[300px] py-[30px] bg-navCloud bg-cover bg-center`}>고만해!</h1>
-         {isListPage&& <> <SearchForm/>
-            <Link href={'/'}><button type="button" className="absolute bottom-[60px] right-[0px]"><Image alt="글 작성하기" src={"/icons/Icon-pencil.svg"} width={30} height={30}/>
-            </button></Link> </>}
+         {isListPage&& 
+         <div className="flex items-center gap-8"> 
+          <SearchForm/>
+          <Link href={'/'}><button type="button" className="group">
+            <Image alt="글 작성하기" src={"/icons/Icon-pencil.svg"} width={30} height={30} className="group-hover:brightness-200 transition duration-300"/>
+          </button></Link> 
+            </div>}
           </div>
         </nav>)
     }
